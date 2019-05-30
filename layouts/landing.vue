c66909bb186fec312138ab1892d03528ec2528fd
dieguischa <dfchacona@unal.edu.co>
2019-05-30 11:43:31 -0500

    :star2: Form and logo :hammer:

--- layouts/landing.vue
+++ layouts/landing.vue
@@ -2,15 +2,32 @@
 <v-container grid-list text-xs-center >
       <v-layout id="register">
               <div class= "left">
-                  Image and app information
+                  <v-container grid-list text-xs-center >
+                    <div id="appImage">
+                    <img src="~assets/images/KWII-LOGO.png" height="200" width="200"/>
+                    <h1>KIWII</h1>
+                    </div>
+                    <div id="appInfo">
+                    <h2>Chatea con tus amigos y más...</h2>
+                    </div>
+                  </v-container>
               </div>
               <div class="rigth">
+                <!--forms-->
               <nuxt/>
               </div>
       </v-layout>
       </v-container>
 </template>
 <style>
+    #appImage{
+        font-family: Helvetica;
+        color: white;
+        margin: 100px 10px 50px 10px;
+    }
+    #appInfo{
+        margin: auto;
+    }
     div.container{
         margin: 0;
         margin-top: 0px;
@@ -27,14 +44,18 @@
         font-family: Arial, Helvetica, sans-serif;
         color: #282828;
         margin: 0px;
-        background: #35693c;
+        background: #c0e4ceec;
         width:30%;
         height: -webkit-fill-available;
+        border-radius: 10px;
+        border-style: dashed;
+        border-color: green;
     }
     div.rigth{
         background: white;
         background-repeat: repeat-y;
         width:70%;
         height: 100%;
+        padding:20px;
     }
 </style>
--- pages/login.vue
+++ pages/login.vue
@@ -1,5 +1,32 @@
 <template>
-      <h1>Formulario de login</h1>
+      <div>
+      <h1>Login</h1>
+      <v-form
+      ref="form"
+      v-model="valid"
+      lazy-validation
+    >
+      <v-text-field
+        v-model="name"
+        :counter="10"
+        :rules="nameRules"
+        label="Nombre de usuario"
+        required
+      ></v-text-field>
+      <v-text-field
+        v-model="email"
+        :rules="emailRules"
+        label="Contraseña"
+        required
+      ></v-text-field>
+      <v-btn
+        color="error"
+        @click="reset"
+      >
+        Login
+      </v-btn>
+    </v-form>
+      </div>
 </template>
 
 <script>
