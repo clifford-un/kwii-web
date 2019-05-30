2a23abd51ad38e83c8c7b185db8950dc1e1bb0df
dieguischa <dfchacona@unal.edu.co>
2019-05-30 10:42:11 -0500

    :hammer:  created landing template

---
+++ layouts/landing.vue
@@ -0,0 +1,40 @@
+<template>
+<v-container grid-list text-xs-center >
+      <v-layout id="register">
+              <div class= "left">
+                  Image and app information
+              </div>
+              <div class="rigth">
+              <nuxt/>
+              </div>
+      </v-layout>
+      </v-container>
+</template>
+<style>
+    div.container{
+        margin: 0;
+        margin-top: 0px;
+        max-width: 100%;
+        padding: 0px;
+    }
+    div.layout{
+        margin: 0;
+        margin-top: 0px;
+        max-width: 100%;
+        padding: 0px;
+    }
+    div.left{
+        font-family: Arial, Helvetica, sans-serif;
+        color: #282828;
+        margin: 0px;
+        background: #35693c;
+        width:30%;
+        height: -webkit-fill-available;
+    }
+    div.rigth{
+        background: white;
+        background-repeat: repeat-y;
+        width:70%;
+        height: 100%;
+    }
+</style>
