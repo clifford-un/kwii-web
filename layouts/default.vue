e57261730b5ed1158ef13d0ff08baf47ff1eb91c
Toño <ajsuarezf@unal.edu.co>
2019-05-30 17:32:16 -0500

    Understanding Nuxt to make chat view

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,31 +7,3 @@
     </v-content>
   </v-app>
 </template>
-
-<script>
-export default {
-  data() {
-    return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'apps',
-          title: 'Welcome',
-          to: '/'
-        },
-        {
-          icon: 'bubble_chart',
-          title: 'Inspire',
-          to: '/inspire'
-        }
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'KWII'
-    }
-  }
-}
-</script>
--- pages/chat.vue
+++ pages/chat.vue
@@ -1,42 +1,93 @@
 <template>
-  <v-layout column justify-center align-center>
-    <v-layout align-start justify-center row fill-height>
-      <v-navigation-drawer v-model="drawer" :mini-variant="miniVariant" :clipped="clipped" fixed app>
-        <v-list>
-          <v-list-tile>
-            <v-toolbar :clipped-left="clipped" fixed app />
+  <v-app id="chat">
+    <v-toolbar color="primary" card dark fixed app>
+      <v-btn icon @click.stop="drawer = !drawer">
+        <v-icon>{{ `chevron_${!drawer ? 'right' : 'left'}` }}</v-icon>
+      </v-btn>
+      <v-toolbar-side-title>
+        <v-avatar>
+          <img :src="selected.avatar">
+        </v-avatar>
+      </v-toolbar-side-title>
+      <v-toolbar-title> {{ selected.name }} </v-toolbar-title>
+      <v-spacer />
+      <v-btn icon>
+        <v-icon>search</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>more_vert</v-icon>
+      </v-btn>
+    </v-toolbar>
+    <v-navigation-drawer v-model="drawer" fixed app>
+      <v-toolbar card color="primary" dark>
+        <v-toolbar-title>KWII</v-toolbar-title>
+        <v-spacer />
+        <v-btn icon>
+          <v-icon>search</v-icon>
+        </v-btn>
+        <v-btn icon>
+          <v-icon>more_vert</v-icon>
+        </v-btn>
+      </v-toolbar>
+      <v-list two-line>
+        <template v-for="(item) in items">
+          <v-list-tile :key="item.name" avatar>
+            <v-list-tile-avatar>
+              <img :src="item.avatar">
+            </v-list-tile-avatar>
+            <v-list-tile-content>
+              <v-list-tile-title>
+                {{ item.name }}
+              </v-list-tile-title>
+              <v-list-tile-sub-title>
+                {{ item.last_message }}
+              </v-list-tile-sub-title>
+            </v-list-tile-content>
           </v-list-tile>
-          <v-list-tile />
-          <v-list-tile v-for="(item, i) in items" :key="i" :to="item" router exact>
-            <img :src="item.photo" alt="">
-            <p>
-              {{ item.name }}
-            </p>
-          </v-list-tile>
-        </v-list>
-      </v-navigation-drawer>
-      <p>
-        hola
-      </p>
-    </v-layout>
-  </v-layout>
+        </template>
+      </v-list>
+    </v-navigation-drawer>
+    <p>
+      hola
+    </p>
+  </v-app>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      items: [
-        {
-          photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUzmcXfpKcca8Y0zQLCpLVohzfk-7yf4pR4NcNEr750mkKiMZDoA',
-          name: 'Toñito'
-        },
-        {
-          photo: 'static/v.png',
-          name: 'Angel'
-        }
-      ]
+  props: {
+    source: {
+      type: String,
+      default: 'lol'
     }
-  }
+  },
+  data: () => ({
+    items: [
+      {
+        avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
+        name: 'Toñito Fortich',
+        last_message: '¿Cómo fueron visajes?'
+      },
+      {
+        avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg',
+        name: 'El otro tipo',
+        last_message: 'Fueron chéveres c:'
+      },
+      {
+        avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg',
+        name: 'Yarid Mujer',
+        last_message: 'Arch >> Manjaro. '
+      }
+    ],
+    selected: {
+      avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg',
+      name: 'Yarid Mujer',
+      last_message: 'Arch >> Manjaro. '
+    },
+    drawer: null,
+    drawerRight: null,
+    right: false,
+    left: false
+  })
 }
 </script>
