dfc3c5ee20d3143e3aaf79195eb21eda58729006
dieguischa <dfchacona@unal.edu.co>
2019-05-30 10:45:05 -0500

    :bowtie: Using landing template :star2:

---
+++ pages/login.vue
@@ -0,0 +1,9 @@
+<template>
+      <h1>Formulario de login</h1>
+</template>
+
+<script>
+export default {
+  layout: 'landing'
+}
+</script>
---
+++ pages/register.vue
@@ -0,0 +1,9 @@
+<template>
+      <h1>Formulario de registro</h1>
+</template>
+
+<script>
+export default {
+  layout: 'landing'
+}
+</script>
