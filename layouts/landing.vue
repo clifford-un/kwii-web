48a77e84aeddb453d8c07b54c8b5dfc7057b6ee2
Angel Corredor <adcorredorm@unal.edu.co>
2019-06-02 12:50:19 -0500

    layout modify

--- layouts/landing.vue
+++ layouts/landing.vue
@@ -1,61 +1,41 @@
 <template>
-<v-container grid-list text-xs-center >
-      <v-layout id="register">
-              <div class= "left">
-                  <v-container grid-list text-xs-center >
-                    <div id="appImage">
-                    <img src="~assets/images/KWII-LOGO.png" height="200" width="200"/>
-                    <h1>KIWII</h1>
-                    </div>
-                    <div id="appInfo">
-                    <h2>Chatea con tus amigos y más...</h2>
-                    </div>
-                  </v-container>
-              </div>
-              <div class="rigth">
-                <!--forms-->
-              <nuxt/>
-              </div>
-      </v-layout>
-      </v-container>
+    <v-layout id="register" class="full-heigth">
+        <v-flex
+        xs5
+        id="chooser"
+        >
+            <v-container grid-list text-xs-center >
+                <div id="appImage">
+                    <img
+                    src="~assets/images/KWII-LOGO.png"
+                    height="200"
+                    width="200"
+                    />
+                    <h1>KWII</h1>
+                </div>
+                <div id="appInfo">
+                    <h2>This is't WhatsApp Web :P</h2>
+                </div>
+            </v-container>
+        </v-flex>
+        <v-flex>
+            <v-container>
+            <!--forms-->
+            <nuxt/>
+            </v-container>
+        </v-flex>
+    </v-layout>
 </template>
-<style>
+<style scoped>
+    #chooser{
+        background-color: green
+    }
     #appImage{
         font-family: Helvetica;
         color: white;
         margin: 100px 10px 50px 10px;
     }
-    #appInfo{
-        margin: auto;
-    }
-    div.container{
-        margin: 0;
-        margin-top: 0px;
-        max-width: 100%;
-        padding: 0px;
-    }
-    div.layout{
-        margin: 0;
-        margin-top: 0px;
-        max-width: 100%;
-        padding: 0px;
-    }
-    div.left{
-        font-family: Arial, Helvetica, sans-serif;
-        color: #282828;
-        margin: 0px;
-        background: #c0e4ceec;
-        width:30%;
+    .full-heigth{
         height: -webkit-fill-available;
-        border-radius: 10px;
-        border-style: dashed;
-        border-color: green;
-    }
-    div.rigth{
-        background: white;
-        background-repeat: repeat-y;
-        width:70%;
-        height: 100%;
-        padding:20px;
     }
 </style>
