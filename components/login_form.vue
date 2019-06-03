8391808015e3a9be6523ac8ea322e5dab5df4d10
Angel Corredor <adcorredorm@unal.edu.co>
2019-06-02 20:20:21 -0500

    Components splitting and register form

---
+++ components/login_form.vue
@@ -0,0 +1,74 @@
+<template>
+  <v-container>
+    <v-form>
+      <v-flex xs6 offset-xs3>
+        <v-text-field
+          v-model="username"
+          v-validate="'required'"
+          prepend-inner-icon="person_pin"
+          :error-messages="errors.collect('username')"
+          label="username"
+          data-vv-name="username"
+          required
+          clearable
+        />
+        <v-text-field
+          v-model="password"
+          v-validate="'required'"
+          :error-messages="errors.collect('password')"
+          :append-icon="show ? 'visibility' : 'visibility_off'"
+          :type="show ? 'text' : 'password'"
+          name="password"
+          label="password"
+          value=""
+          @click:append="show = !show"
+        />
+        <center>
+          <v-btn
+            round
+            large
+            color="green"
+            @click="submit"
+          >
+            sign In
+          </v-btn>
+          <v-btn
+            round
+            large
+            color="orange"
+            @click="register"
+          >
+            sign Up
+          </v-btn>
+        </center>
+      </v-flex>
+    </v-form>
+  </v-container>
+</template>
+
+<script>
+import Vue from 'vue'
+import VeeValidate from 'vee-validate'
+
+Vue.use(VeeValidate)
+
+export default {
+  data: () => {
+    return {
+      username: '',
+      password: '',
+      show: false
+    }
+  },
+  methods: {
+    submit() {
+      this.username = ''
+      this.password = ''
+    },
+    register() {
+      this.show = !this.show
+      this.$emit('update:register', true)
+    }
+  }
+}
+</script>
--- components/register_form.vue
+++ components/register_form.vue
@@ -1,10 +1,11 @@
 <template>
   <v-container>
-    <form>
+    <v-form>
       <v-flex xs6 offset-xs3>
         <v-text-field
           v-model="username"
           v-validate="'required'"
+          prepend-inner-icon="person_pin"
           :error-messages="errors.collect('username')"
           label="username"
           data-vv-name="username"
@@ -14,6 +15,7 @@
         <v-text-field
           v-model="password"
           v-validate="'required'"
+          prepend-inner-icon="visibility"
           :error-messages="errors.collect('password')"
           :append-icon="show ? 'visibility' : 'visibility_off'"
           :type="show ? 'text' : 'password'"
@@ -22,31 +24,60 @@
           value=""
           @click:append="show = !show"
         />
-        <div>
+        <v-text-field
+          v-model="email"
+          v-validate="'required'"
+          prepend-inner-icon="email"
+          :error-messages="errors.collect('email')"
+          name="email"
+          label="email"
+          value=""
+        />
+        <v-text-field
+          v-model="phone"
+          v-validate="'required'"
+          prepend-inner-icon="phone"
+          :error-messages="errors.collect('phone')"
+          name="phone"
+          label="phone"
+          value=""
+        />
+        <center>
           <v-btn
             round
             large
-            color="green"
+            color="orange"
             @click="submit"
           >
-            login
+            Submit
           </v-btn>
-        </div>
+          <v-btn
+            round
+            large
+            color="red"
+            @click="register"
+          >
+            Back
+          </v-btn>
+        </center>
       </v-flex>
-    </form>
+    </v-form>
   </v-container>
 </template>
 
 <script>
 import Vue from 'vue'
 import VeeValidate from 'vee-validate'
+
 Vue.use(VeeValidate)
+
 export default {
-  layout: 'landing',
   data: () => {
     return {
       username: '',
       password: '',
+      email: '',
+      phone: '',
       show: false
     }
   },
@@ -54,6 +85,9 @@ export default {
     submit() {
       this.username = ''
       this.password = ''
+    },
+    register() {
+      this.$emit('update:register', false)
     }
   }
 }
