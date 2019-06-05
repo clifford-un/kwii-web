e2f0a0104b39c6e035019f3e57fd14c18eb6f7e4
Toño <ajsuarezf@unal.edu.co>
2019-06-05 13:19:56 -0500

    Forms are now outside

--- components/session/Login.vue
+++ components/session/Login.vue
@@ -1,18 +1,19 @@
-<component @update_form="update_form" />
 <template>
   <v-app>
     <v-container>
       <v-content>
-        <registerForm v-if="register" :register.sync="register" />
-        <loginForm v-else :register.sync="register" />
+        <v-layout column>
+          <registerForm v-if="register" :register.sync="register" />
+          <loginForm v-else :register.sync="register" />
+        </v-layout>
       </v-content>
     </v-container>
   </v-app>
 </template>
 
 <script>
-import LoginForm from '~/components/session/LoginForm.vue'
-import RegisterForm from '~/components/session/RegisterForm.vue'
+import LoginForm from '~/components/session/form/LoginForm.vue'
+import RegisterForm from '~/components/session/form/RegisterForm.vue'
 
 export default {
   components: {
