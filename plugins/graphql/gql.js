164960c6ccf9aa10afe59d99b0af33b2ad5d2eed
Fortich <ajsuarezf@unal.edu.co>
2019-07-13 19:57:43 -0500

    Update gql.js

--- plugins/graphql/gql.js
+++ plugins/graphql/gql.js
@@ -3,12 +3,12 @@ import { request } from 'graphql-request'
 export default {
   data: () => {
     return {
-      url: 'https://34.73.50.226/kwii_api/graphql'
+      url: 'https://www.kwii.com/kwii_api/graphql'
     }
   },
   methods: {
     async request(query) {
-      const ans = await request('https://34.73.50.226/kwii_api/graphql', query)
+      const ans = await request('https://www.kwii.com/kwii_api/graphql', query)
       return ans
     }
   }
